2620a7dae9c72030d9c0ff98e04db324d53050ac
Dominic Galiano <dgaliano@utexas.edu>
2023-09-25 16:09:52 -0400

    initialize backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
