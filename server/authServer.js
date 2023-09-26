bddd582a2f04c38d04049ed15c50cd653751c264
Dominic Galiano <dgaliano@utexas.edu>
2023-09-26 15:42:19 -0400

    refactored token generation

--- server/authServer.js
+++ server/authServer.js
@@ -11,17 +11,15 @@ const user = {
 
 app.use(express.json());
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   // rudimentary authenticate user
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res
-      .status(400)
-      .json({
-        error: "Bad Request",
-        message: "Username and password are required.",
-      });
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "Username and password are required.",
+    });
   }
 
   if (!authenticateUser(username, password)) {
@@ -31,26 +29,16 @@ app.post("/login", (req, res) => {
   }
 
   // create JWT
-  (async () => {
-    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
-    const alg = "HS256";
-
-    try {
-      const jwt = await new jose.SignJWT({ name: username })
-        .setProtectedHeader({ alg })
-        .setIssuedAt()
-        .setExpirationTime("2h")
-        .sign(secret);
-
-      res.status(200).json({ accessToken: jwt });
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({
-        error: "Internal Server Error",
-        message: "An error occurred while generating the JWT.",
-      });
-    }
-  })();
+  try {
+    const jwt = await generateJWT(username);
+    res.status(200).json({ accessToken: jwt });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+      message: "An error occurred while generating the JWT.",
+    });
+  }
 });
 
 app.listen(port, () => {
@@ -59,4 +47,22 @@ app.listen(port, () => {
 
 function authenticateUser(username, password) {
   return !(username !== user.username || password !== user.password);
+}
+
+async function generateJWT(username) {
+  {
+    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
+    const alg = "HS256";
+
+    try {
+      return (jwt = await new jose.SignJWT({ name: username })
+        .setProtectedHeader({ alg })
+        .setIssuedAt()
+        .setExpirationTime("2h")
+        .sign(secret));
+    } catch (error) {
+      console.error("Error:", error);
+      throw new Error("An error occurred while generating JWT");
+    }
+  }
 }
