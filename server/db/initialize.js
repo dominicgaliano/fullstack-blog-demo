bf75b0546970f115339002f5def4b853a5dc1776
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 15:14:11 -0400

    changed init code

--- server/db/initialize.js
+++ server/db/initialize.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.ATLAS_URI)
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bepzz2s.mongodb.net/?retryWrites=true&w=majority`
+  )
   .then(() => {
     console.log("mongodb connected...");
   })
