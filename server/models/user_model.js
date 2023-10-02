0f4b5b69c5944ed2a30ba773c32c40870badf75e
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 16:59:49 -0400

    explicitly named user collection

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const isEmail = require("validator").isEmail;
 
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: "Email address is required",
-    validate: [isEmail, "Please fill a valid email address"],
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: "Email address is required",
+      validate: [isEmail, "Please fill a valid email address"],
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
   },
-  hashedPassword: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: "users" }
+);
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
