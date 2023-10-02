735b0851f929e1754764aa9d777d8cd3d60d0a15
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 16:58:22 -0400

    changed db name

--- server/db/initialize.js
+++ server/db/initialize.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bepzz2s.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bepzz2s.mongodb.net/fullstack-app?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("mongodb connected.");
