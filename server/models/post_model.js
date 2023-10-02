7602d887d057c8830d51680a5fdf5e3a9b54b9a1
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 17:37:44 -0400

    Post Schema

---
+++ server/models/post_model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const isEmail = require("validator").isEmail;
+
+const PostSchema = new Schema(
+  {
+    author: {
+      user_id: Number,
+      email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        validate: [isEmail, "Please fill a valid email address"],
+      },
+    },
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+    likes: Number,
+    comments: [
+      {
+        comment_id: Number,
+        author: {
+          user_id: mongoose.ObjectId,
+          email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            validate: [isEmail, "Please fill a valid email address"],
+          },
+        },
+        text: String,
+      },
+    ],
+  },
+  { collection: "posts" }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
