e7cac0549380054358fbf58099ac011e0dbc320a
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 21:51:52 -0400

    moved auth routes to own file

--- server/index.js
+++ server/index.js
@@ -1,52 +1,18 @@
-const { verifyToken } = require("./util/auth");
-const { getPosts, createPost } = require("./util/posts");
-const { getUserById } = require("./util/users");
 const express = require("express");
 const morgan = require("morgan");
-const createError = require("http-errors");
+
 const app = express();
 const port = 3001;
+
+// initialize database
 require("./db/initialize");
 
+// middleware
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.get("/", verifyToken, async (req, res) => {
-  // fetch posts from server
-  let posts;
-  try {
-    posts = await getPosts();
-  } catch (error) {
-    console.log("Error:", error);
-    return res.status(500).json({
-      error: "Internal Server Error",
-      message: "An error occurred while fetching posts",
-    });
-  }
-
-  res.status(200).json(posts);
-});
-
-app.post("/", verifyToken, async (req, res, next) => {
-  try {
-    const user_id = req.user_id;
-    const user = await getUserById(user_id);
-    if (!user) {
-      throw createError(500, "User not found");
-    }
-
-    const content = req.body.content;
-    if (!content) {
-      throw createError(400, "No post content found");
-    }
-
-    const post = await createPost(user, content);
-
-    res.status(201).json(post);
-  } catch (err) {
-    next(err);
-  }
-});
+// routes
+app.use("/", require("./routes"));
 
 // error handler
 app.use(async (err, req, res, next) => {
