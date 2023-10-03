8c0d5f8a0f535dc9966b274c799ab17a79c9e249
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 22:10:15 -0400

    moved logic to controllers

---
+++ server/controllers/index.js
@@ -0,0 +1,42 @@
+const { getPosts, createPost } = require("../util/posts");
+const { getUserById } = require("../util/users");
+const createError = require("http-errors");
+
+const getPostsController = async (req, res) => {
+  // fetch posts from server
+  let posts;
+  try {
+    posts = await getPosts();
+  } catch (error) {
+    console.log("Error:", error);
+    return res.status(500).json({
+      error: "Internal Server Error",
+      message: "An error occurred while fetching posts",
+    });
+  }
+
+  res.status(200).json(posts);
+};
+
+const createPostController = async (req, res, next) => {
+  try {
+    const user_id = req.user_id;
+    const user = await getUserById(user_id);
+    if (!user) {
+      throw createError(500, "User not found");
+    }
+
+    const content = req.body.content;
+    if (!content) {
+      throw createError(400, "No post content found");
+    }
+
+    const post = await createPost(user, content);
+
+    res.status(201).json(post);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getPostsController, createPostController };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,46 +1,11 @@
 const { verifyToken } = require("../util/auth");
-const { getPosts, createPost } = require("../util/posts");
-const { getUserById } = require("../util/users");
-const createError = require("http-errors");
+const { getPostsController, createPostController } = require("../controllers");
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/posts", verifyToken, async (req, res) => {
-  // fetch posts from server
-  let posts;
-  try {
-    posts = await getPosts();
-  } catch (error) {
-    console.log("Error:", error);
-    return res.status(500).json({
-      error: "Internal Server Error",
-      message: "An error occurred while fetching posts",
-    });
-  }
+router.get("/posts", verifyToken, getPostsController);
 
-  res.status(200).json(posts);
-});
-
-router.post("/posts", verifyToken, async (req, res, next) => {
-  try {
-    const user_id = req.user_id;
-    const user = await getUserById(user_id);
-    if (!user) {
-      throw createError(500, "User not found");
-    }
-
-    const content = req.body.content;
-    if (!content) {
-      throw createError(400, "No post content found");
-    }
-
-    const post = await createPost(user, content);
-
-    res.status(201).json(post);
-  } catch (err) {
-    next(err);
-  }
-});
+router.post("/posts", verifyToken, createPostController);
 
 module.exports = router;
