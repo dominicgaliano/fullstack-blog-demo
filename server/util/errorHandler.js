05758afeb47ed7e718a863b43c436a48cf090a29
Dominic Galiano <dgaliano@utexas.edu>
2023-10-04 12:03:26 -0400

    errorHandler behavior change based on NODE_ENV

--- server/util/errorHandler.js
+++ server/util/errorHandler.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+
 const errorHandler = async (err, req, res, next) => {
-  console.log(err);
+  const environment = process.env.NODE_ENV || "development";
   res.status(err.status || 500);
+
+  if (environment === "development") {
+    console.log(err);
+    return res.send({
+      error: {
+        status: err.status || 500,
+        message: err.message,
+      },
+    });
+  }
+
   res.send({
     error: {
       status: err.status || 500,
-      message: err.message,
     },
   });
 };
