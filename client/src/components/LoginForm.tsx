b0531e54edf5daa5f5a9aad4e0a9d518e0bff140
Dominic Galiano <dgaliano@utexas.edu>
2023-10-05 18:28:19 -0400

    install react-hook-form and create login form

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,28 @@
+import { useForm } from 'react-hook-form';
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Email"
+        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        {...register('Password', { required: true, min: 8, maxLength: 100 })}
+      />
+
+      <input type="submit" />
+    </form>
+  );
+}
