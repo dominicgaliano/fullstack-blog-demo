f746746729f4d9f9521adae7976f5e015ef59858
Dominic Galiano <dgaliano@utexas.edu>
2023-10-09 11:30:41 -0400

    add redux and setup root reducer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,20 @@
 import './index.css';
 
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import rootReducer from './reducers';
+
+const store = configureStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
