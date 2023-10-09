80fd42460a8f62e502e066ec1e4c08cc7ef43bc9
Dominic Galiano <dgaliano@utexas.edu>
2023-10-09 12:07:55 -0400

    dummy req auth

--- client/src/util/auth.ts
+++ client/src/util/auth.ts
@@ -1,11 +1,16 @@
+import axios from 'axios';
+
+import { AUTH_URL } from '../config';
 import LoginInput from '../types/LoginInput';
 import Token from '../types/Token';
 
 export const loginUser = async (loginInput: LoginInput) => {
   // TODO: implement actual login
-  console.log('logging user in:', loginInput.email);
-  await sleep(1000);
-  console.log('logged in');
+  // console.log('logging user in:', loginInput.email);
+  // await sleep(1000);
+  // console.log('logged in');
+  const res = await axios.post(`${AUTH_URL}/login`, loginInput);
+  console.log(res);
 
   const authToken: Token = '';
   const refreshToken: Token = '';
