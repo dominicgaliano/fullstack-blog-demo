479465f4ac41ffb9493af336cb9165fd3072e249
Dominic Galiano <dgaliano@utexas.edu>
2023-10-09 11:50:03 -0400

    loginUser returns blank tokens

--- client/src/util/auth.ts
+++ client/src/util/auth.ts
@@ -1,10 +1,19 @@
 import LoginInput from '../types/LoginInput';
+import Token from '../types/Token';
 
 export const loginUser = async (loginInput: LoginInput) => {
+  // TODO: implement actual login
   console.log('logging user in:', loginInput.email);
   await sleep(1000);
   console.log('logged in');
-  return true;
+
+  const authToken: Token = '';
+  const refreshToken: Token = '';
+
+  return {
+    authToken: authToken,
+    refreshToken: refreshToken,
+  };
 };
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
