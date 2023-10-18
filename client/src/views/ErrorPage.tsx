df4e07b2aea80b84fe31c6fd89becf73ce976861
Dominic Galiano <dgaliano@utexas.edu>
2023-10-18 10:02:13 -0400

    fix error page link

--- client/src/views/ErrorPage.tsx
+++ client/src/views/ErrorPage.tsx
@@ -7,7 +7,7 @@ function ErrorPage() {
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p>{/* <i>{errorMessage(error)}</i> */}</p>
-        <Link to={`/feed`}>Return Home</Link>
+        <Link to={`/`}>Return Home</Link>
       </div>
     </>
   );
