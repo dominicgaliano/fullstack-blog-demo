dd06e2448a2a4d0590d6610600d5a7c9fe15f7ce
Dominic Galiano <dgaliano@utexas.edu>
2023-10-18 09:28:44 -0400

    add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,17 @@ import { Route, Routes } from 'react-router-dom';
 
 import ErrorPage from './views/ErrorPage';
 import Feed from './views/Feed';
+import Home from './views/Home';
 import Login from './views/Login';
+import Register from './views/Register';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
--- client/src/views/ErrorPage.tsx
+++ client/src/views/ErrorPage.tsx
@@ -1,34 +1,34 @@
-import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+// import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  // const error = useRouteError();
+  // console.error(error);
 
   return (
     <>
       <div id="error-page">
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{errorMessage(error)}</i>
-        </p>
+        <p>{/* <i>{errorMessage(error)}</i> */}</p>
         <Link to={`/feed`}>Return Home</Link>
       </div>
     </>
   );
 }
 
-function errorMessage(error: unknown): string {
-  if (isRouteErrorResponse(error)) {
-    return `${error.status} ${error.statusText}`;
-  } else if (error instanceof Error) {
-    return error.message;
-  } else if (typeof error === 'string') {
-    return error;
-  } else {
-    console.error(error);
-    return 'Unknown error';
-  }
-}
+// function errorMessage(error: unknown): string {
+//   if (isRouteErrorResponse(error)) {
+//     return `${error.status} ${error.statusText}`;
+//   } else if (error instanceof Error) {
+//     return error.message;
+//   } else if (typeof error === 'string') {
+//     return error;
+//   } else {
+//     console.error(error);
+//     return 'Unknown error';
+//   }
+// }
 
 export default ErrorPage;
