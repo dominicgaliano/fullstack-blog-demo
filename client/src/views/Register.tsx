43abc4ec2e316b6ea102b18f771d7a1bfb24ff1c
Dominic Galiano <dgaliano@utexas.edu>
2023-10-19 08:20:48 -0400

    login register centered

--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,12 +1,16 @@
+import './Login.css';
+
 import { Link } from 'react-router-dom';
 
 import LoginRegisterForm from '../components/LoginRegisterForm';
 
 const Register = () => {
   return (
-    <div>
-      <LoginRegisterForm login={false} />
-      <Link to="/login">login</Link>
+    <div className="form-container">
+      <div className="form-child">
+        <LoginRegisterForm login={false} />
+        <Link to="/login">login</Link>
+      </div>
     </div>
   );
 };
