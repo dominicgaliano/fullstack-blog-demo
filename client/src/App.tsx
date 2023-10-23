b8bc4cf9a93c94904e1f9e6750d3f2ecd4a5eb3a
Dominic Galiano <dgaliano@utexas.edu>
2023-10-23 15:13:08 -0400

    add spin loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,12 @@ import { Route, Routes } from 'react-router-dom';
 
 import { loadUser } from './actions/authActions';
 import { useAppDispatch } from './app/hooks';
+import SpinLoader from './components/SpinLoader';
 import PrivateRoute from './routes/PrivateRoute';
 import CreatePostView from './views/CreatePostView';
 import ErrorPage from './views/ErrorPage';
 import Feed from './views/Feed';
 import Home from './views/Home';
-import LoadingView from './views/LoadingView';
 import Login from './views/Login';
 import PostView from './views/PostView';
 import Register from './views/Register';
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <>
-      <Suspense fallback={<LoadingView />}>
+      <Suspense fallback={<SpinLoader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
---
+++ client/src/components/SpinLoader.tsx
@@ -0,0 +1,5 @@
+import './SpinLoader.css';
+
+export default function SpinLoader() {
+  return <div className={'loader-container'}></div>;
+}
