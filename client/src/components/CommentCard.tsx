f1658949c4f04d5a3651af021bc33f9ed06953e6
Dominic Galiano <dgaliano@utexas.edu>
2023-10-25 12:41:02 -0400

    add edit delete buttons

--- client/src/components/CommentCard.tsx
+++ client/src/components/CommentCard.tsx
@@ -2,8 +2,11 @@ import CommentType from '../types/CommentType';
 
 export default function CommentCard({ comment }: { comment: CommentType }) {
   return (
-    <>
-      {comment.author.email}: {comment.text}
-    </>
+    <div>
+      {comment.author.email}
+      {comment.text}
+      <button>EDIT</button>
+      <button>DELETE</button>
+    </div>
   );
 }
