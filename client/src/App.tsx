7f5bc9b82e05735bd3cda9a42c08eb32620e95d0
Dominic Galiano <dgaliano@utexas.edu>
2023-11-07 15:40:25 -0500

    Remove old css, add MUI CssBaseline component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 import { lazy, Suspense, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
-import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import * as material from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -19,6 +19,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <material.CssBaseline />
         <App />
       </BrowserRouter>
     </Provider>
